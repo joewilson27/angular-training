a766c0d738a1b6364cbde5a200b7c6b86f3a6a47
Joe Wilson <wilsonmangunsong@gmail.com>
2022-06-27 22:43:34 +0700

    sec7_vid32_to_vid38
    Sec 7 Vid 32
    Behind the Scenes of An Angular Project
    <app-root></app-root> --> replace this tag / selector with template of the componenr (similar to an "include")
    
    @Component is an Angular "Decotator" similar to Annotation in Java (you know it)
    
    General files
    sales-person-list.component.ts --> the component class
    sales-person-list.component.html --> the component template HTML
    sales-person-list.component.css --> the component private css
    sales-person-list.component.spec.ts --> the unit test specifications
    
    Sec 7 Vid 34
    Generate New Component with CLI
    ng generate component sales-person-list
    
    Sec 7 Vid 35
    Generate Class with CLI
    ng generate class sales-person-list/SalesPerson
    
    In Angular World, developers commonly use "public" properties / access modifier as their variable

--- my-first-angular-project/src/app/app.component.ts
+++ my-first-angular-project/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+// @Component similar to Annotation in Java world
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', // this is for selector in html <app-root></app-root>
+  templateUrl: './app.component.html', // file html to be rendered
+  styleUrls: ['./app.component.css'] 
 })
 export class AppComponent {
-  title = 'my-first-angular-project';
+  title = 'This is My First App';
+
+  firstName = 'Bill';
+  lastName = 'Smith';
 }
---
+++ sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+
+  // create an array of objects
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Benjamin", "Madden", "benjamin.madden@gmail.com", 50000),
+    new SalesPerson("John", "Doe", "john.doe@gmail.com", 40000),
+    new SalesPerson("Claire", "Murphy", "claire.murphy@gmail.com", 90000),
+    new SalesPerson("May", "Truong", "may.truong@gmail.com", 60000)
+  ];
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
